1b829166f3145b4ba52e229497450fb8c5b6cec3
diemurillo <diemurillo976@gmail.com>
2020-10-12 21:59:26 -0600

    cambios menores

--- mammotrck_repo/mammotrck/main/Clients/ClientFactory.py
+++ mammotrck_repo/mammotrck/main/Clients/ClientFactory.py
@@ -1,8 +1,14 @@
 from .Client_list import clients
 
 def get_client(request):
-    #Cambiar peticiones url para implementar cliente mediante el parametro "client", en vez de "android=1"
-    for client in clients:
-        if (request.GET.get(client) or request.POST.get(client)):
-            return clients[client]()
-    return clients["web"]()
+    #Peticiones url implementan el cliente mediante el parametro "client", por ejemplo "client=android"
+    type = None
+    if (request.GET.get('client')):
+        type = request.GET.get('client')
+    elif (request.POST.get('client')):
+        type = request.POST.get('client')
+
+    if (type in clients):
+        return clients[client]()
+        
+    return None
